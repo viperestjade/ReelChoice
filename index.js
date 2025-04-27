5fee74f56a4fd44e4ea8d79c0459b49b40fe5df9
Rosella Jade Asetre Ricafort <ricafortjade@gmail.com>
2025-04-27 21:16:30 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-document.getElementById("loginForm").addEventListener("submit", function(event){
+document.getElementById("loginForm").addEventListener("submit", function(event) {
     event.preventDefault(); // Stop page from refreshing
 
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
 
     // Example hardcoded email and password
-    if(email === "admin@email.com" && password === "1234") {
-        window.location.href = "ReelChoiceHomepage.html"; // teleport to homepage
+    if (email === "admin@email.com" && password === "1234") {
+        window.location.href = "ReelChoiceHomepage/ReelChoiceHomepage.html"; 
     } else {
         alert("Incorrect email or password.");
     }
