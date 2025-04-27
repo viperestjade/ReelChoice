8c69926f16297ff7fe2cbe11510a7aa6b8fdf8b8
Rosella Jade Asetre Ricafort <ricafortjade@gmail.com>
2025-04-27 21:21:53 +0800

    Add files via upload

---
+++ ReelChoiceHomepage/ReelChoiceHomepage.js
@@ -0,0 +1,8 @@
+document.getElementById("surpriseBtn").addEventListener("click", function() {
+    alert("Here's a surprise movie recommendation!");
+  });
+  
+  document.getElementById("filtersBtn").addEventListener("click", function() {
+    alert("Let's apply some filters to find your movie!");
+  });
+  
