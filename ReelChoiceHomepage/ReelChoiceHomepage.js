187c7eb6cfc320fccd911fbf8469fdea919b1046
Rosella Jade Asetre Ricafort <ricafortjade@gmail.com>
2025-04-29 23:52:55 +0800

    Delete ReelChoiceHomepage directory

--- ReelChoiceHomepage/ReelChoiceHomepage.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("surpriseBtn").addEventListener("click", function() {
-    alert("Here's a surprise movie recommendation!");
-  });
-  
-  document.getElementById("filtersBtn").addEventListener("click", function() {
-    alert("Let's apply some filters to find your movie!");
-  });
-  
