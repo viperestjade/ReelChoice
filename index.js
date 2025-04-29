d76ce38fed3cb61450797bd3f36229f34743f187
Rosella Jade Asetre Ricafort <ricafortjade@gmail.com>
2025-04-30 00:11:31 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 document.getElementById("surpriseBtn").addEventListener("click", function() {
-    alert("Here's a surprise movie recommendation!");
-  });
-  
-  document.getElementById("filtersBtn").addEventListener("click", function() {
-    alert("Let's apply some filters to find your movie!");
-  });
-  
+  alert("Here's a surprise movie recommendation!");
+});
+
+document.getElementById("filtersBtn").addEventListener("click", function() {
+  alert("Let's apply some filters to find your movie!");
+});
