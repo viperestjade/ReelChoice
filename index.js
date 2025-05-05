72fafcf7cee0b347e85ed0f951aa6ce87836df7e
Rosella Jade Asetre Ricafort <ricafortjade@gmail.com>
2025-05-06 01:05:35 +0800

    Update index.js

--- index.js
+++ index.js
@@ -10,3 +10,7 @@ document.querySelector('.menu-icon').addEventListener('click', function () {
   document.getElementById('sideMenu').classList.toggle('active');
 });
 
+function closeMenu() {
+  document.getElementById('sideMenu').classList.remove('active');
+}
+
